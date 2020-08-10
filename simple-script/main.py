b065922ba416cc7c0abfad81c2b84b82aa5f7da5
Miguel Rasero <miguel@linos.es>
2020-08-10 11:46:33 +0200

    first commit

---
+++ simple-script/main.py
@@ -0,0 +1,11 @@
+import pycharm_package.models
+
+
+def main():
+    # db_map = pycharm_package.models.database_map
+    # fixed_rules = pycharm_package.models.globals
+    pass
+
+
+if __name__ == '__main__':
+    main()
